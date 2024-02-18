621f78a3bdbdd018cc74563871ca99ffd5e9e618
James Gould <nobleburgundy@gmail.com>
2024-02-18 09:29:02 -0600

    Initial commit.

---
+++ BoloGame.cs
@@ -0,0 +1,10 @@
+namespace bolo;
+
+public class BoloGame
+{
+    public DateTime Date { get; set; }
+
+    public string Winner { get; set; }
+
+    public string[] Players { get; set; }
+}
---
+++ Controllers/GamesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bolo.Controllers;
+
+[ApiController]
+[Route("api/games")]
+public class GamesController : ControllerBase
+{
+    private static readonly string[] Players = new[]
+    {
+        "James", "Laura", "Luther", "Sonia", "Andrew", "Raquel", "Bob"
+    };
+
+    private readonly ILogger<GamesController> _logger;
+
+    public GamesController(ILogger<GamesController> logger)
+    {
+        _logger = logger;
+        _logger.LogInformation("pastGames called");
+    }
+
+    [HttpGet]
+    public IEnumerable<BoloGame> Get()
+    {
+        Console.WriteLine("pastGames called");
+        _logger.LogWarning("pastGames called");
+        return Enumerable.Range(1, 5).Select(index => new BoloGame
+        {
+            Date = DateTime.Now.AddDays(-index),
+            Winner = Players[Random.Shared.Next(Players.Length)],
+            Players = Players.Take(Random.Shared.Next(4, Players.Length)).ToArray()
+        })
+        .ToArray();
+    }
+}
