c489c2475b9672ed03b7b08b048647bfdae82450
James Gould <nobleburgundy@gmail.com>
2024-03-11 15:43:17 -0500

    Fixed docker communication issues

--- Bolo/ClientApp/src/environments/environment.prod.ts
+++ Bolo/ClientApp/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://backend:5000/api', // Use backend as the hostname
+  apiUrl: 'http://localhost:5000/api', // Use backend as the hostname
 };
